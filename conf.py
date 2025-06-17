206141389edd99473a0864abcab7ff853d7304eb
johncheever <contact@avgxhelp.com>
2025-06-17 10:26:28 +0530

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,22 @@
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+project = 'Roadrunner Email Help Center'
+author = 'Support Team'
+release = '1.0'
+copyright = '2025'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
