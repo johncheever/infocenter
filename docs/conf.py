123415a48ee4634d16a6d892d671932fa659ebbb
johncheever <contact@avgxhelp.com>
2025-06-18 12:22:20 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,8 +4,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Sbcglobal Email Help Center'
-author = 'Support Team'
+project = 'More Sbcglobal'
+author = 'Rose Anderson'
 release = '1.0'
 copyright = '2025'
 
